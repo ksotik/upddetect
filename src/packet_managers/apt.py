aefb966391e5375cecef5ae20c10a7c2048367d1
ksotik <>
2023-01-09 11:41:07 +0100

    apt implementation

--- src/packet_managers/apt.py
+++ src/packet_managers/apt.py
@@ -7,6 +7,7 @@ import subprocess
 class AptPacketManager(PacketManager):
 
     apt_path = ""
+    apt_cmd = ""
 
     def __repr__(self) -> str:
         return "APT"
@@ -17,10 +18,11 @@ class AptPacketManager(PacketManager):
             return p.stdout.decode('utf-8')
 
         self.apt_path = which('apt-get')
+        if self.apt_path:
+            self.apt_cmd = "apt-get"
         if not self.apt_path:
             self.apt_path = which('apt')
-        if not self.apt_path:
-            self.apt_path = which('aptitude')
+            self.apt_cmd = "apt"
 
         if self.apt_path:
             return True
@@ -28,18 +30,14 @@ class AptPacketManager(PacketManager):
             return False
 
     def __detect(self, dist: bool = False, only_security: bool = False) -> list:
-        if self.apt_path.find('apt-get') >= 0:
-            p1 = subprocess.Popen(['apt-get', '-s', 'dist-upgrade' if dist else 'upgrade', '-V'],
+        if self.apt_path:
+            p1 = subprocess.Popen([self.apt_cmd, '-s', 'dist-upgrade' if dist else 'upgrade', '-V'],
                                   stdout=subprocess.PIPE)
             if only_security:
                 p2 = subprocess.run(['awk', '/^Inst.*security/ {print $2}'], stdin=p1.stdout, stdout=subprocess.PIPE)
             else:
                 p2 = subprocess.run(['awk', '/^Inst/ {print $2}'], stdin=p1.stdout, stdout=subprocess.PIPE)
             return p2.stdout.decode('utf-8').strip().split('\n')
-        elif self.apt_path.find('aptitude') >= 0:
-            pass  # TODO
-        elif self.apt_path.find('apt') >= 0:
-            pass  # TODO
         return []
 
     def detect_updates(self, only_security: bool = False) -> list:
