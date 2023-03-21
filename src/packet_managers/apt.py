481bd58d823304cbcd251895cff9911c902bca8a
ksotik <>
2023-03-21 22:59:06 +0100

    First ready-to-use version

--- src/packet_managers/apt.py
+++ src/packet_managers/apt.py
@@ -7,19 +7,23 @@ import subprocess
 class AptPacketManager(PacketManager):
 
     def __repr__(self) -> str:
-        return "APT"
+        return self.pm_path if self.pm_path else "APT"
+
+    @staticmethod
+    def get_human_name() -> str:
+        return "APT (apt-get, apt)"
+
+    @staticmethod
+    def get_type() -> str:
+        return "apt"
 
     def is_available(self) -> bool:
         if PacketManager.is_available(self):
             return True
 
-        def which(s: str) -> str:
-            p = subprocess.run(['which', s], stdout=subprocess.PIPE)
-            return p.stdout.decode('utf-8').strip()
-
-        self.pm_path = which('apt-get')
+        self.pm_path = self.which('apt-get')
         if not self.pm_path:
-            self.pm_path = which('apt')
+            self.pm_path = self.which('apt')
 
         if self.pm_path:
             return True
@@ -31,16 +35,24 @@ class AptPacketManager(PacketManager):
 
     def __detect(self, dist: bool = False, only_security: bool = False) -> (str, list):
         if self.pm_path:
-            p1 = subprocess.run([self.pm_path, '-s', 'dist-upgrade' if dist else 'upgrade', '-V'],
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if p1.stderr:
-                # TODO: обработка ошибки
+            p = subprocess.run([self.pm_path, '-s', 'dist-upgrade' if dist else 'upgrade', '-V'],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if p.stderr:
+                self.print_error(p.stderr.decode("utf-8"))
                 return self.pm_path, []
-            if only_security:
-                p2 = subprocess.run(['awk', '/^Inst.*security/ {print $2}'], input=p1.stdout, stdout=subprocess.PIPE)
-            else:
-                p2 = subprocess.run(['awk', '/^Inst/ {print $2}'], input=p1.stdout, stdout=subprocess.PIPE)
-            return self.pm_path, p2.stdout.decode('utf-8').strip().split('\n')
+            out = p.stdout.decode('utf-8').strip().split('\n')
+            res = []
+            for s in out:
+                s = s.split(" ")
+                if len(s) < 5:
+                    continue
+                package = s[1]
+                if s[0] == "Inst":
+                    curr_ver = s[2][1:-1]
+                    new_ver = s[3][1:]
+                    if not only_security or "security" in s[4] or "security" in s[5]:
+                        res.append(self.make_result_line(package, curr_ver, new_ver, "", self.colorize))
+            return self.pm_path, res
         else:
             return None, []
 
